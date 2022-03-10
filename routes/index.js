75ae99728f0dec9969f4d6ab96d792cd6becf0cd
gurra404 <gustafgronlund@gmail.com>
2022-03-10 21:08:37 +0100

    Updating for heroku

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const auth = require('../middlewares/auth');
 
 /* GET / */
-router.get('/', (req, res, next) => {
-    res.send({ success: true, data: { msg: 'oh, hi' } });
-});
+// router.get('/', (req, res, next) => {
+//     res.send({ success: true, data: { msg: 'oh, hi' } });
+// });
 
 router.use('/register', require('./users'));
 router.use('/photos', auth.basicUserAuth, require('./photos'));
